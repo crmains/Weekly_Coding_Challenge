8df00e17bdc42a12515deb8bbdcc6adcfa606827
ChristianMains <crmains@protonmail.com>
2022-04-27 11:19:04 -0500

    finished

--- weekly_coding_challenge/src/robot_in_a_grid/Robot_in_a_grid.java
+++ weekly_coding_challenge/src/robot_in_a_grid/Robot_in_a_grid.java
@@ -1,30 +1,64 @@
 package robot_in_a_grid;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Robot_in_a_grid {
 
 	public static void main(String[] args) {
-		int[] robot = new int[2];
-		robot[0] = 0; robot [1] = 0;
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("What is the number of rows in the grid.\n>");
+		System.out.print("What is the number of rows in the grid.\n>");
 		int rows = scan.nextInt();
+		//rows = 100;
 		
-		System.out.println("What is the number of columns in the grid.\n>");
+		System.out.print("What is the number of columns in the grid.\n>");
 		int colmns = scan.nextInt();
+		//colmns = 100;
 		
-		int[][] grid = new int[rows][colmns];
+		int[][] grid = new int[rows][colmns];  // create Grid
 		
-		for(int i = 0; i < rows; i++) {
-			for(int j = 0; j < colmns; j++) {
-				System.out.print(grid[i][j]);
-			}
-			System.out.print("\n");
+		for(int i = 0; i < rows; i++) { //  Add obstablce to Grid
+			Random rand = new Random();
+			int random = rand.nextInt(colmns-1) +1;
+			grid[i][random] = 1;
 		}
+		
+//		for(int i = 0; i < rows; i++) {       // Print out Grid so we can see it
+//			for(int j = 0; j < colmns; j++) {
+//				System.out.print(grid[i][j]);
+//			}
+//			System.out.print("\n");
+//		}
+		/*
+		 *  Note: the lower number of grids and columns you go the greater
+		 *  chance that the Robot will fail.
+		 */
+		robot_path(0, 0, grid);
 
 	}
-
+	
+	public static void robot_path(int robotRow, int robotColmn, int[][] grid) {
+		int timer = 0;
+		int time = grid.length* 2;
+		while(robotRow < grid.length-1 || robotColmn < grid[0].length-1) {
+			if(timer == time) {break;}
+			if(robotColmn < grid[0].length-1 && grid[robotRow][robotColmn + 1] == 0) {
+				robotColmn++;
+			}
+			if(robotRow < grid.length-1 && grid[robotRow +1][robotColmn] == 0) {
+				robotRow++;
+			}
+			timer++;
+		}
+		//   The Robots ending location, uncomment to see
+		//System.out.print("Robot Location "+ robotRow + " "+ robotColmn+"\n");
+		if(robotRow == grid.length-1 && robotColmn == grid[0].length-1) {
+			System.out.print("Robot has reached Distination");
+		}else {
+			System.out.print("The Robot has Failed!");
+		}
+	}
+	
 }
